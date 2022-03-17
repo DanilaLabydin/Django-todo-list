3bfc323b333dd2e63744f62a8001bcdd38af6f29
danilalabydindfsdf <labydin2605@mail.ru>
2022-03-17 14:31:40 +0300

    create a first class view

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.views.generic import ListView
+from .models import ToDoList
 
-# Create your views here.
+
+# the ListListView class will display a list of the to-do list titles
+class ListListView(ListView):
+    model = ToDoList
+    template_name = "todo_app/index.html"
