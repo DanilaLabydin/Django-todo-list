59188cb32bfdb8df88ac9e89048d0c841a4d70f6
danilalabydindfsdf <labydin2605@mail.ru>
2022-03-17 16:27:13 +0300

    add an anothor view and add url patterns

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,3 +1,9 @@
-urlpatterns = [
+from django.urls import path
+from . import views
 
+urlpatterns = [
+    path('',
+         views.ListView.as_view(), name='index'),
+    path('list/<int:list_id>/',
+         views.ItemListView.as_view(), name='list')
 ]
