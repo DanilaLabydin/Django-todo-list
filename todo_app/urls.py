32e1934bee9636795b1171a2c70896a6d7627c79
danilalabydindfsdf <labydin2605@mail.ru>
2022-03-18 17:00:42 +0300

    fix bugs in this files, add a proper view class in urls.py

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',
-         views.ListView.as_view(), name='index'),
-    path('list/<int:list_id>/',
-         views.ItemListView.as_view(), name='list')
+    path('', views.ListListView.as_view(), name='index'),
+    path('list/<int:list_id>/', views.ItemListView.as_view(), name='list')
 ]
